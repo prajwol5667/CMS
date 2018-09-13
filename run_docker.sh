27243b0b47554a2ab5d3ffe1e378b43bee3327b4
prajwol5667 <prajwol_shakya200@outlook.com>
2018-09-13 11:14:08 +0545

    Shell script for CMS

---
+++ run_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Creating image"
+if [ $# -ne 1 ];
+    then echo "illegal number of parameters"
+    exit 1
+fi
+
+BUILD_NUM=$1
+echo "Running with BUILD_NUM=" $BUILD_NUM
+docker build -t prajwol5667/cms:$BUILD_NUM .
+docker run -d --name t1  --rm -p 8080:8099 prajwol5667/cms:$BUILD_NUM
